05ae765c36d28c7f93a4f9559a352149c26cb8e0
Kathryn-cat <jinqic@andrew.cmu.edu>
2023-01-30 11:21:16 -0800

    constructMultiBatchGEMM test

--- tests/python/relax/test_transform_cutlass_codegen.py
+++ tests/python/relax/test_transform_cutlass_codegen.py
@@ -54,6 +54,9 @@ def build(mod):
     return executable
 
 
+# basic tests
+
+
 def constructGEMM(m, n, k, GLOBAL_SYMBOL="HGEMM"):
     from tvm.script.ir_builder import IRBuilder
     from tvm.script.ir_builder import ir as I
@@ -474,14 +477,78 @@ def test_cutlass_batch_dense2_bias():
     np.testing.assert_allclose(result.numpy(), A @ B + bias, rtol=1e-2)
 
 
+# manually constructed test cases for multi-dim batch workloads and loop permutation
+
+# einsum "ghij, ghjk -> ghik"
+def constructMultiBatchGEMM(b1, b2, m, n, k, GLOBAL_SYMBOL="MultiBatchHGEMM"):
+    from tvm.script.ir_builder import IRBuilder
+    from tvm.script.ir_builder import ir as I
+    from tvm.script.ir_builder import relax as R
+    from tvm.script.ir_builder import tir as T
+
+    with IRBuilder() as ib:  # pylint: disable=invalid-name
+        with I.ir_module() as frame:
+            with T.prim_func():
+                T.func_name(GLOBAL_SYMBOL)
+                T.func_attr(
+                    {
+                        "global_symbol": GLOBAL_SYMBOL,
+                    }
+                )
+                A = T.arg(
+                    "A", T.buffer_decl((b1, b2, m, k), A_TYPE)
+                )  # pylint: disable=invalid-name
+                B = T.arg(
+                    "B", T.buffer_decl((b1, b2, k, n), B_TYPE)
+                )  # pylint: disable=invalid-name
+                D = T.alloc_buffer((b1, b2, m, n), C_TYPE)
+                with T.grid(b1, b2, m, n, k) as (lb1, lb2, l0, l1, l2):
+                    with T.block("multi_batch_dense_row_row_row"):
+                        vb1, vb2, vi, vj, vk = T.axis.remap("SSSSR", [lb1, lb2, l0, l1, l2])
+                        T.reads(A[vb1, vb2, vi, vk], B[vb1, vb2, vk, vj])
+                        T.writes(D[vb1, vb2, vi, vj])
+                        with T.init():
+                            T.buffer_store(D, T.cast(0.0, C_TYPE), [vb1, vb2, vi, vj])
+                        T.buffer_store(
+                            D,
+                            D[vb1, vb2, vi, vj] + A[vb1, vb2, vi, vk] * B[vb1, vb2, vk, vj],
+                            [vb1, vb2, vi, vj],
+                        )
+            with R.function():
+                R.func_name("main")
+                A = R.arg("A", R.tensor((b1, b2, m, k), A_TYPE))  # pylint: disable=invalid-name
+                B = R.arg("B", R.tensor((b1, b2, k, n), B_TYPE))  # pylint: disable=invalid-name
+                C = R.call_tir(
+                    frame.global_vars[GLOBAL_SYMBOL],
+                    args=[A, B],
+                    shape=(b1, b2, m, n),
+                    dtype=C_TYPE,
+                )
+                R.func_ret_value(C)
+    mod = ib.get()
+    return mod
+
+
+def test_cutlass_multi_batch_dense():
+    b1, b2, m, n, k = 2, 3, 128, 128, 128
+    build(constructMultiBatchGEMM(b1, b2, m, n, k))
+    dev = tvm.cuda()
+    A = np.random.rand(b1, b2, m, k).astype("float16") * 5
+    B = np.random.rand(b1, b2, k, n).astype("float16") * 5
+    A_tvm = tvm.nd.array(A, dev)
+    B_tvm = tvm.nd.array(B, dev)
+    executable = tvm.runtime.load_module(PKG_FILE)
+    result = f_run(executable, dev, A_tvm, B_tvm)
+    np.testing.assert_allclose(result.numpy(), A @ B, rtol=1e-2)
+
+
 if __name__ == "__main__":
-    test_cutlass_dense()
+    # test_cutlass_dense()
+    # test_cutlass_dense_bias()
+    # test_cutlass_dense_bias_relu()
+    # test_cutlass_batch_dense()
+    # test_cutlass_batch_dense2()
+    # test_cutlass_batch_dense_bias()
+    # test_cutlass_batch_dense2_bias()
+    test_cutlass_multi_batch_dense()
     print("passed test")
-    """
-    test_cutlass_dense_bias()
-    test_cutlass_dense_bias_relu()
-    test_cutlass_batch_dense()
-    test_cutlass_batch_dense2()
-    test_cutlass_batch_dense_bias()
-    test_cutlass_batch_dense2_bias()
-    """
