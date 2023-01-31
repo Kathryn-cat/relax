c2df3ef8354afbfe5259b45b27ad5ed8a6e88d63
Kathryn-cat <jinqic@andrew.cmu.edu>
2023-01-30 20:13:57 -0800

    constructPermutationGEMM test

--- tests/python/relax/test_transform_cutlass_codegen.py
+++ tests/python/relax/test_transform_cutlass_codegen.py
@@ -1029,6 +1029,65 @@ def test_cutlass_reduction_dense4():
     np.testing.assert_allclose(result.numpy(), np.einsum("ghij, ghjk->ik", A, B), rtol=1e-2)
 
 
+# einsum "hij, ijk -> hik"
+def constructPermutationGEMM(b, m, n, k, GLOBAL_SYMBOL="PermutationHGEMM"):
+    from tvm.script.ir_builder import IRBuilder
+    from tvm.script.ir_builder import ir as I
+    from tvm.script.ir_builder import relax as R
+    from tvm.script.ir_builder import tir as T
+
+    with IRBuilder() as ib:  # pylint: disable=invalid-name
+        with I.ir_module() as frame:
+            with T.prim_func():
+                T.func_name(GLOBAL_SYMBOL)
+                T.func_attr(
+                    {
+                        "global_symbol": GLOBAL_SYMBOL,
+                    }
+                )
+                A = T.arg("A", T.buffer_decl((b, m, k), A_TYPE))  # pylint: disable=invalid-name
+                B = T.arg("B", T.buffer_decl((m, k, n), B_TYPE))  # pylint: disable=invalid-name
+                D = T.alloc_buffer((b, m, n), C_TYPE)
+                with T.grid(b, m, n, k) as (lb, l0, l1, l2):
+                    with T.block("permutation_dense_row_row_row"):
+                        vb, vi, vj, vk = T.axis.remap("SSSR", [lb, l0, l1, l2])
+                        T.reads(A[vb, vi, vk], B[vi, vk, vj])
+                        T.writes(D[vb, vi, vj])
+                        with T.init():
+                            T.buffer_store(D, T.cast(0.0, C_TYPE), [vb, vi, vj])
+                        T.buffer_store(
+                            D,
+                            D[vb, vi, vj] + A[vb, vi, vk] * B[vi, vk, vj],
+                            [vb, vi, vj],
+                        )
+            with R.function():
+                R.func_name("main")
+                A = R.arg("A", R.tensor((b, m, k), A_TYPE))  # pylint: disable=invalid-name
+                B = R.arg("B", R.tensor((m, k, n), B_TYPE))  # pylint: disable=invalid-name
+                C = R.call_tir(
+                    frame.global_vars[GLOBAL_SYMBOL],
+                    args=[A, B],
+                    shape=(b, m, n),
+                    dtype=C_TYPE,
+                )
+                R.func_ret_value(C)
+    mod = ib.get()
+    return mod
+
+
+def test_cutlass_permutation_dense():
+    b, m, n, k = 2, 128, 128, 128
+    assert build(constructPermutationGEMM(b, m, n, k)), "build failure on CUDA"
+    dev = tvm.cuda()
+    A = np.random.rand(b, m, k).astype("float16") * 5
+    B = np.random.rand(m, k, n).astype("float16") * 5
+    A_tvm = tvm.nd.array(A, dev)
+    B_tvm = tvm.nd.array(B, dev)
+    executable = tvm.runtime.load_module(PKG_FILE)
+    result = f_run(executable, dev, A_tvm, B_tvm)
+    np.testing.assert_allclose(result.numpy(), np.einsum("hij, ijk->hik", A, B), rtol=1e-2)
+
+
 if __name__ == "__main__":
     # test_cutlass_dense()
     # test_cutlass_dense_bias()
@@ -1045,5 +1104,6 @@ if __name__ == "__main__":
     # test_cutlass_reduction_dense()
     # test_cutlass_reduction_dense2()
     # test_cutlass_reduction_dense3()
-    test_cutlass_reduction_dense4()
+    # test_cutlass_reduction_dense4()
+    test_cutlass_permutation_dense()
     print("passed test")
